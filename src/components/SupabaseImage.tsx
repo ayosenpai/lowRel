a5a942a0054899b64f887f033d0c5ad8590a9752
Minga Developer <deploy@minga.com>
2026-07-08 04:00:52 +0530

    Add SupabaseImage component

---
+++ src/components/SupabaseImage.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import Image, { ImageProps } from "next/image";
+import supabaseLoader from "@/lib/supabase-image-loader";
+
+// Client-side wrapper that applies supabaseLoader safely.
+// Use this instead of plain <Image> for Supabase product bucket images
+// in Server Components where passing loader as a prop would cause a hydration error.
+export default function SupabaseImage(props: Omit<ImageProps, "loader">) {
+    return <Image loader={supabaseLoader} {...props} />;
+}
